c460455c44295533c5366b7b060c273642469411
blueback <blueback28@gmail.com>
2021-12-21 16:55:48 +0900

    0.0.6 debugview_gameobject / nullチェック

--- BlueBackGl/Assets/UPM/Runtime/BlueBack/Gl/SpriteIndex.cs
+++ BlueBackGl/Assets/UPM/Runtime/BlueBack/Gl/SpriteIndex.cs
@@ -55,18 +55,20 @@ namespace BlueBack.Gl
 			this.spritelist = a_spritelist;
 
 			#if(DEF_BLUEBACK_GL_DEBUGVIEW)
-			this.debugview_gameobject = new UnityEngine.GameObject("sprite");
-			Sprite_DebugView_MonoBehaviour t_debugview_monobehaviour = this.debugview_gameobject.AddComponent<Sprite_DebugView_MonoBehaviour>();
-			this.debugview_gameobject.SetActive(false);
+			{
+				this.debugview_gameobject = new UnityEngine.GameObject("sprite");
+				Sprite_DebugView_MonoBehaviour t_debugview_monobehaviour = this.debugview_gameobject.AddComponent<Sprite_DebugView_MonoBehaviour>();
+				this.debugview_gameobject.SetActive(false);
 
-			#if(DEF_BLUEBACK_GL_DEBUGVIEW_VIEWALL)
-			#else
-			this.debugview_gameobject.hideFlags = UnityEngine.HideFlags.HideInHierarchy;
-			#endif
+				#if(DEF_BLUEBACK_GL_DEBUGVIEW_VIEWALL)
+				#else
+				this.debugview_gameobject.hideFlags = UnityEngine.HideFlags.HideInHierarchy;
+				#endif
 
-			UnityEngine.GameObject.DontDestroyOnLoad(this.debugview_gameobject);
-			t_debugview_monobehaviour.spritelist = a_spritelist;
-			t_debugview_monobehaviour.spriteindex = this;
+				UnityEngine.GameObject.DontDestroyOnLoad(this.debugview_gameobject);
+				t_debugview_monobehaviour.spritelist = a_spritelist;
+				t_debugview_monobehaviour.spriteindex = this;
+			}
 			#endif
 		}
 
@@ -75,8 +77,10 @@ namespace BlueBack.Gl
 		public void Dispose()
 		{
 			#if(DEF_BLUEBACK_GL_DEBUGVIEW)
-			UnityEngine.GameObject.Destroy(this.debugview_gameobject);
-			this.debugview_gameobject = null;
+			if(this.debugview_gameobject != null){
+				UnityEngine.GameObject.Destroy(this.debugview_gameobject);
+				this.debugview_gameobject = null;
+			}
 			#endif
 		}
 
@@ -85,7 +89,9 @@ namespace BlueBack.Gl
 		#if(DEF_BLUEBACK_GL_DEBUGVIEW)
 		public void SetDebugName(string a_debugname)
 		{
-			this.debugview_gameobject.name = a_debugname;
+			if(this.debugview_gameobject != null){
+				this.debugview_gameobject.name = a_debugname;
+			}
 		}
 		#endif
 
@@ -94,19 +100,16 @@ namespace BlueBack.Gl
 		#if(DEF_BLUEBACK_GL_DEBUGVIEW)
 		public void SetDebugActive(bool a_flag)
 		{
-			if(a_flag == true){
-				this.debugview_gameobject.SetActive(true);
+			if(this.debugview_gameobject != null){
+				this.debugview_gameobject.SetActive(a_flag);
 
 				#if(DEF_BLUEBACK_GL_DEBUGVIEW_VIEWALL)
 				#else
-				this.debugview_gameobject.hideFlags = UnityEngine.HideFlags.None;
-				#endif
-			}else{
-				this.debugview_gameobject.SetActive(false);
-
-				#if(DEF_BLUEBACK_GL_DEBUGVIEW_VIEWALL)
-				#else
-				this.debugview_gameobject.hideFlags = UnityEngine.HideFlags.HideInHierarchy;
+				if(a_flag == true){
+					this.debugview_gameobject.hideFlags = UnityEngine.HideFlags.None;
+				}else{
+					this.debugview_gameobject.hideFlags = UnityEngine.HideFlags.HideInHierarchy;
+				}
 				#endif
 			}
 		}
