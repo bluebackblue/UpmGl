3a0263e44b9bf63ec590969000a7c294ffef3292
blueback <blueback28@gmail.com>
2022-08-15 21:24:43 +0900

    更新 : VersionManager

--- BlueBackGl/Assets/UPM/Runtime/BlueBack/Gl/Version.cs
+++ BlueBackGl/Assets/UPM/Runtime/BlueBack/Gl/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.Gl
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.21";
+		public const string packageversion = "0.0.22";
 
 		/** GetPackageVersion
 		*/
