087f7b3571777980ee1573686db3420c097090ab
blueback <blueback28@gmail.com>
2022-08-16 21:06:39 +0900

    整理

--- BlueBackGl/Assets/UPM/Runtime/BlueBack/Gl/Version.cs
+++ BlueBackGl/Assets/UPM/Runtime/BlueBack/Gl/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.Gl
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.23";
+		public const string packageversion = "0.0.24";
 
 		/** GetPackageVersion
 		*/
